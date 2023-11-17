8f57142819f0caccc5edeb77e7f80a7aff7bdade
G09 <g09@G09ui-iMac.local>
2023-11-17 18:07:33 +0900

    montyHall

--- MontyHall.py
+++ MontyHall.py
@@ -5,3 +5,14 @@ no_change = 0
 change = 0
 
 trial = 100000
+
+for _ in range(trial):
+    car = random.randint(0,2)
+    player_choice = random.randint(0,2)
+
+    empty_door[]
+    for i in range(3):
+        if i!=player_choice and i != car:
+            empty_door.append(door[i])
+
+    com = random.sample(empty_door, 1)[0]
