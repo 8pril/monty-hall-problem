c710a287c75daf1ed2593aeacf7f64db2dde07a5
Soyoung Lee <sycj427@gmail.com>
2023-11-17 18:13:41 +0900

    feat: Add Feature #2

--- MontyHall.py
+++ MontyHall.py
@@ -5,3 +5,12 @@ no_change = 0
 change = 0
 
 trial = 100000
+
+for _ in range(trial):
+    car = random.randint(0,2)
+    player_choice = random.randint(0,2)
+    
+    if player_choice == car:
+        no_change += 1
+
+print("선택을 바꾸지 않은 경우:", no_change/trial, "%")
