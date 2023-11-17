02563a515747132769503fae6889eef1366420fa
G09 <g09@G09ui-iMac.local>
2023-11-17 18:09:34 +0900

    montyHallFix

--- MontyHall.py
+++ MontyHall.py
@@ -10,7 +10,7 @@ for _ in range(trial):
     car = random.randint(0,2)
     player_choice = random.randint(0,2)
 
-    empty_door[]
+    empty_door=[]
     for i in range(3):
         if i!=player_choice and i != car:
             empty_door.append(door[i])
