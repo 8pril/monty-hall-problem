9d327a13c7cea556776b645d8b43d4df2546d2b2
coolcutt3r <erondite@gmail.com>
2023-11-17 18:08:25 +0900

    feat: add choose not to change logic

--- MontyHall.py
+++ MontyHall.py
@@ -5,3 +5,29 @@ no_change = 0
 change = 0
 
 trial = 100000
+
+for _ in range(trial):
+    car = random.randint(0,2)
+    player_choice = random.randint(0,2)
+
+    empty_door = []
+    #사회자가 선택하기 위한 경우의 수를 추리기 위해
+    for i in range(3):
+        if i !=player_choice and i != car:
+            empty_door.append(door[i])
+
+    com = random.sample(empty_door, 1)[0]
+
+    # 선택을 바꾸지 않은 경우
+    if player_choice == car:
+        no_change += 1
+    
+
+
+
+
+
+
+
+print("선택을 바꾸지 않은 경우:", no_change/trial, "%")
+print("선택을 바꾼 경우:", change/trial, "%")
